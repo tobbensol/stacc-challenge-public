cef1bcd9d8af6fb3cc9e9271c334ff9c21a94637
tobbensol <tobsol01@gmail.com>
2023-10-21 11:17:22 +0200

    added info gathering

--- src/user_interface.py
+++ src/user_interface.py
@@ -1,39 +1,75 @@
-from tinydb import TinyDB
-from Account import Accounts
-from Transaction import Transactions
+import datetime
+from locale import currency
+from random import randint
+import Account
+from Backend import Backend
 
 class UserInterface:
-    def __init__(self, accounts: Accounts, transactions: Transactions):
-        self.accounts: Accounts = accounts
-        self.transactions: Transactions = transactions
+    def __init__(self, backend: Backend):
+        self.backend = backend
 
     def start(self):
         while True:
-            choice = input("1. Add Account\n2. Make Transaction\n3. Plot Account Activity\n4. Quit\nEnter your choice: ")
+            choice = input("\n1. Add Account\n2. Make Transaction\n3. Plot Account Activity\n4. Quit\nEnter your choice: ")
 
             if choice == '1':
                 account_data = self.get_account_data_from_user()
-                self.accounts.add_account(account_data)
+                self.backend.accounts.add_account(account_data)
+                print(f"Successfully made account with the ID: {account_data['id']}")
 
             elif choice == '2':
                 transaction_data = self.get_transaction_data_from_user()
-                self.transactions.make_transaction(transaction_data)
+                self.backend.make_transaction(transaction_data)
 
             elif choice == '3':
                 account_id = input("Enter account ID: ")
-                self.accounts.plot_account_activity(account_id, self.transactions.transactions)
+                self.backend.plot_account_activity(account_id)
 
             elif choice == '4':
                 break
 
-    def get_account_data_from_user(self):
-        # Implement code to get account data from the user (e.g., through input)
-        pass
-
     def get_transaction_data_from_user(self):
         # Implement code to get transaction data from the user (e.g., through input)
-        pass
+        id_str = str(randint(0, 999))
+        id = "txn" + (3-len(id_str)) * "0" + id_str
+        description = input("Description: ")
+        amount = eval(input("Amount: ")) #TODO: safeEval
+        currency = input("Currency: ")
+        account_id = input("Account_ID: ")
 
-    def make_transaction(self, transaction: dict) -> None:
-        self.transactions.add_transactions(transaction)
-        self.accounts.transaction(transaction)
+        while True:
+            date_str = input("Date (YYYY-MM-DD): ")
+            try:
+                date = datetime.strptime(date_str, "%Y-%m-%d")
+                break  # Exit the loop if a valid date is provided
+            except ValueError:
+                print("Invalid date format. Please enter the date in YYYY-MM-DD format.")
+        
+        return({
+            "id": id,
+            "date": date,
+            "description": description,
+            "amount": amount,
+            "currency": currency,
+            "account_id": account_id
+        })
+
+    def get_account_data_from_user(self):
+        # Implement code to get account data from the user (e.g., through input)
+        id_str = str(randint(0, 999))
+        id = "acc" + (3-len(id_str)) * "0" + id_str
+        account_str = str(randint(0, 9999))
+        account_number = "*"*8 + (4-len(account_str)) * "0" + account_str
+        account_type = input("AccountType: ")
+        balance = eval(input("Amount: ")) #TODO: safeEval
+        currency = input("Currency: ")
+        owner = input("Owner: ")
+        
+        return({
+            "id": id,
+            "account_number": account_number,
+            "account_type": account_type,
+            "balance": balance,
+            "currency": currency,
+            "owner": owner
+        })
