4b041a6561c53cc8180e683e24be73d7143e4424
tobbensol <tobsol01@gmail.com>
2023-10-21 11:43:05 +0200

    deleted old code

--- src/Account.py
+++ src/Account.py
@@ -27,57 +27,6 @@ class Accounts():
 
     def get_account(self, account_id: str):
         return self.accounts.get(Query().id == account_id)
-    
-    def get_balance_history(self, account_id: str, transactions: TinyDB.table_class):
-        #Gather info
-        account = self.get_account(account_id)
-        if not account:
-            print("Account not found.")
-            return None
-        
-        account_balance = account["balance"]
-        transaction_data = transactions.search(Query().account_id == account_id)
-
-        #Handle no transactions
-        if not transaction_data: 
-            #Should idealy be [account creation date, today]
-            return [datetime.now()-timedelta(days=1), datetime.now()], [account_balance]*2
-        
-        #Handle dates
-        sorted_transactions = sorted(transaction_data, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%d"),)
-        timestamps = [datetime.strptime(transaction["date"], "%Y-%m-%d") for transaction in sorted_transactions]
-        before_first_transaction = timestamps[0] - timedelta(days=1)
-        timestamps.insert(0, before_first_transaction)
-
-        #Handle balances
-        balances = [account_balance]
-        for transaction in reversed(sorted_transactions):
-            account_balance -= transaction["amount"]
-            balances.insert(0, account_balance)
-        
-        return timestamps, balances
-
-    def plot_account_activity(self, account_id: str, transactions: TinyDB.table_class):
-        timestamps, balances = self.get_balance_history(account_id, transactions)
-
-        #Linear regression
-        days_since_first = [(date - timestamps[0]).days for date in timestamps]
-        slope, intercept = np.polyfit(days_since_first, balances, 1)
-        regression_line = slope * np.array(days_since_first) + intercept
-
-        # Create a line chart to visualize the account balance over time
-        plt.figure(figsize=(10, 5))
-        plt.plot(timestamps, balances, marker='o')
-        plt.plot(timestamps, regression_line, 'r-', label='Linear Regression Line')
-        plt.title(f"Account Activity for Account ID {account_id}")
-        plt.xlabel("Date")
-        plt.ylabel("Account Balance (NOK)")
-        plt.legend(["Balance", "Estimate"])
-        plt.grid(True)
-
-        # Display the chart or save it to a file
-        plt.show()
-
 
     def __repr__(self):
         string = ""
