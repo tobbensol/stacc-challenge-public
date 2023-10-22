5345153ee82fc6a7ed2311c50ca8973d2b34ab89
Tobias Soltvedt <tobsol01@gmail.com>
2023-10-22 19:29:50 +0200

    restructured user interface

--- src/user_interface.py
+++ src/user_interface.py
@@ -22,23 +22,22 @@ class UserInterface:
 Enter your choice: 
 """)
             match choice:
-                case "1":
+                case "1":  # Add Account
                     account_data = get_account_data_from_user()
                     self.backend.accounts.add_account(account_data)
-                    # if I used python 3.12, I would be able to use " " in the f-string :c
                     print(f"Successfully made {account_data['account_type']} account with the ID: {account_data['id']}")
-                case "2":
+                case "2":  # Make Transaction
                     transaction_data = self.get_transaction_data_from_user()
                     if self.backend.make_transaction(transaction_data):
                         print("Transaction was successful!")
-                case "3":
+                case "3":  # Plot Account Activity
                     account_id = get_id_from_user(self.backend.accounts.accounts, "Enter account ID: ")
                     self.backend.plot_account_activity(account_id)
-                case "4":
+                case "4":  # Manage Savings
                     self.savings()
-                case "5":
+                case "5":  # Print DB
                     print(self.backend)
-                case "6":
+                case "6":  # Quit
                     break
                 case _:
                     print("Please enter a valid option")
@@ -48,40 +47,40 @@ Enter your choice:
             choice = input("""
 1. Add savings account
 2. Transfer to savings account
-3. See saving accounts
+3. Plot estimates
 4. Change monthly payment
 5. Make monthly payments
-6. Plot estimates
+6. See saving accounts
 7. Quit\n
 Enter your choice: 
 """)
             match choice:
-                case "1":
+                case "1":  # Add savings account
                     savings_data = self.get_savings_account_from_user()
                     self.backend.saving_goals.add_saving_goal(savings_data)
                     print(
                         f"Successfully made savings account for \"{savings_data['name']}\" with the ID: {savings_data['id']}")
-                case "2":
+                case "2":  # Transfer to savings account
                     savings_account, transaction = self.get_savings_transfer_from_user()
                     self.backend.transfer_to_savings(savings_account, transaction)
-                case "3":
+                case "3":  # Plot estimates
+                    account_id = get_id_from_user(self.backend.saving_goals.saving_goals, "Enter savings ID: ")
+                    self.backend.saving_goals.plot_saving_goal(account_id)
+                case "4":  # Change monthly payment
+                    account_id = get_id_from_user(self.backend.saving_goals.saving_goals, "Enter savings ID: ")
+                    monthly_payment = get_numeric_from_user("Enter new monthly payment: ", True)
+                    self.backend.saving_goals.set_monthly_payment(account_id, monthly_payment)
+                case "5":  # Make monthly payments
+                    date = get_date_from_user()
+                    self.backend.make_monthly_saving(date)
+                case "6":  # See saving accounts
                     account_id = get_id_from_user(self.backend.accounts.accounts, "Enter account ID: ")
                     accounts = self.backend.saving_goals.get_user_saving_goals(account_id)
                     if accounts:
                         print(self.saving_to_str(account_id, accounts))
                     else:
                         print(f"{account_id} has no saving accounts tied to it")
-                case "4":
-                    account_id = get_id_from_user(self.backend.saving_goals.saving_goals, "Enter savings ID: ")
-                    monthly_payment = get_numeric_from_user("Enter new monthly payment: ", True)
-                    self.backend.saving_goals.set_monthly_payment(account_id, monthly_payment)
-                case "5":
-                    date = get_date_from_user()
-                    self.backend.make_monthly_saving(date)
-                case "6":
-                    account_id = get_id_from_user(self.backend.saving_goals.saving_goals, "Enter savings ID: ")
-                    self.backend.saving_goals.plot_saving_goal(account_id)
-                case "7":
+                case "7":  # Quit
                     break
                 case _:
                     print("Please enter a valid option")
